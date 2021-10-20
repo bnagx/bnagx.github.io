38bc93b8d34a1f411ad26d46a8c1d390d8a34bb2
Your NAME <wanhaoxin1006@e.newera.edu.my>
2021-10-20 17:20:16 +0800

    finalize homework

---
+++ week2/homework1.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous" />
+</head>
+
+<body>
+    <h2>What is your date of birth? </h2>
+
+    <div class="btn-group">
+        <button class="btn btn-primary btn-lg dropdown-toggle" data-bs-toggle="dropdown" type="button" aria-expanded="false">
+            Day
+        </button>
+        <ul class="dropdown-menu">
+            <?php
+            $day = 1;
+
+            for ($day = 1; $day < 32; $day++) {
+                echo "<li><a class='dropdown-item fs-6'>" . $day . "</a></li>";
+            }
+            ?>
+        </ul>
+    </div>
+
+    <div class="btn-group">
+        <button class="btn btn-warning btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Month
+        </button>
+        <ul class="dropdown-menu">
+            <?php
+            $month = 0;
+            for ($month = 1; $month <= 12; $month++) {
+
+                echo "<li><a class='dropdown-item fs-6'>" . $month . "</a></li>";
+            }
+            ?>
+        </ul>
+    </div>
+
+    <div class="btn-group">
+        <button class="btn btn-danger btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Year
+        </button>
+        <ul class="dropdown-menu">
+            <?php
+            $year = 1900;
+            for ($year = 1900; $year <= 2021; $year++) {
+
+                echo "<li><a class='dropdown-item fs-6'>" . $year . "</a></li>";
+            }
+            ?>
+        </ul>
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+
+
+
+</body>
+
+</html>
