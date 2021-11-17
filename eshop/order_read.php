9ce6d86c7c907f26677e97c8298af55b90af8e0b
Your NAME <wanhaoxin1006@e.newera.edu.my>
2021-11-17 17:24:32 +0800

    add order_read and order_read_one

--- eshop/order_read.php
+++ eshop/order_read.php
@@ -3,7 +3,7 @@
 
 <head>
     <title>
-        Read Customer
+        Read Product
     </title>
     <!-- Latest compiled and minified Bootstrap CSS -->
     <meta charset="utf-8">
@@ -12,6 +12,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
+
 
 <body>
     <nav class="navbar navbar-inverse">
@@ -23,17 +24,18 @@
                 <li><a href="product_create.php">Create Products</a></li>
                 <li><a href="order_create.php">Create Order</a></li>
                 <li><a href="contactus.php">Contact Us</a></li>
-                <li class="active"><a href="customer_read.php">Read Customers</a></li>
+                <li><a href="customer_read.php">Read Customers</a></li>
                 <li><a href="product_read.php">Read Products</a></li>
-                <li><a href="order_read.php">Read Orders</a></li>
+                <li class="active"><a href="order_read.php">Read Orders</a></li>
             </ul>
         </div>
     </nav>
 
+
     <!-- container -->
     <div class="container">
         <div class="page-header">
-            <h1>Read Customers</h1>
+            <h1>Read Orders</h1>
         </div>
 
         <?php
@@ -43,7 +45,7 @@
         // delete message prompt will be here
 
         // select all data
-        $query = "SELECT username,email,first_name,last_name,gender,dateofbirth,registration,accountstatus FROM customers ORDER BY first_name DESC";
+        $query = "SELECT order_id, username, order_date FROM order_summary ORDER BY order_id DESC";
         $stmt = $con->prepare($query);
         $stmt->execute();
 
@@ -51,7 +53,7 @@
         $num = $stmt->rowCount();
 
         // link to create record form
-        echo "<a href='create_customer.php' class='btn btn-primary m-b-1em'>Create New Customer</a>";
+        echo "<a href='order_create.php' class='btn btn-primary m-b-1em'>Create New Order</a>";
 
         //check if more than 0 record found
         if ($num > 0) {
@@ -60,15 +62,9 @@
 
             //creating our table heading
             echo "<tr>";
-            echo "<th>Username</th>";
-            echo "<th>Email</th>";
-            echo "<th>First Name</th>";
-            echo "<th>Last Name</th>";
-            echo "<th>Gender</th>";
-            echo "<th>Date of Birth</th>";
-            echo "<th>Register Date & Time</th>";
-            echo "<th>Account Status</th>";
-
+            echo "<th>order_id</th>";
+            echo "<th>username</th>";
+            echo "<th>order_date</th>";
             echo "</tr>";
 
             // retrieve our table contents
@@ -78,23 +74,18 @@
                 extract($row);
                 // creating new table row per record
                 echo "<tr>";
+                echo "<td>{$order_id}</td>";
                 echo "<td>{$username}</td>";
-                echo "<td>{$email}</td>";
-                echo "<td>{$first_name}</td>";
-                echo "<td>{$last_name}</td>";
-                echo "<td>{$gender}</td>";
-                echo "<td>{$dateofbirth}</td>";
-                echo "<td>{$registration}</td>";
-                echo "<td>{$accountstatus}</td>";
+                echo "<td>{$order_date}</td>";
                 echo "<td>";
                 // read one record
-                echo "<a href='customer_read_one.php?id={$username}' class='btn btn-info m-r-1em'>Read</a>";
+                echo "<a href='order_read_one.php?id={$order_id}' class='btn btn-info m-r-1em'>Read</a>";
 
                 // we will use this links on next part of this post
-                echo "<a href='customer_update.php?id={$username}' class='btn btn-primary m-r-1em'>Edit</a>";
+                echo "<a href='order_update.php?id={$order_id}' class='btn btn-primary m-r-1em'>Edit</a>";
 
                 // we will use this links on next part of this post
-                echo "<a href='#' onclick='delete_customer({$username});'  class='btn btn-danger'>Delete</a>";
+                echo "<a href='#' onclick='delete_order({$order_id});'  class='btn btn-danger'>Delete</a>";
                 echo "</td>";
                 echo "</tr>";
             }
