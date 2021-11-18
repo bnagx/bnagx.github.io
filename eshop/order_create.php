24f102dfa708803c160ec79f0a1b5bc973d273b9
Your NAME <wanhaoxin1006@e.newera.edu.my>
2021-11-18 12:41:33 +0800

    edit order_create

--- eshop/order_create.php
+++ eshop/order_create.php
@@ -42,9 +42,15 @@
 
 
         $q = "SELECT product_id, name, price FROM products";
-
         $stmt = $con->prepare($q);
         $stmt->execute();
+
+        $product_arrID = array();
+        $product_arrName = array();
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            array_push($product_arrID, $row['product_id']);
+            array_push($product_arrName, $row['name']);
+        }
 
         $cus_username = "SELECT username FROM customers";
 
@@ -56,19 +62,35 @@
             // include database connection
             include 'config/database.php';
 
+            // echo count($_POST['product']); This is to display how many product(Array) count 
+
             $flag = 0;
             $message = '';
+            $pflag = 0;
+            $pflagfail = 0;
 
+            for ($count1 = 0; $count1 < count($_POST['product']); $count1++) {
+                if (!empty($_POST['quantity'][$count1]) && !empty($_POST['product'][$count1])) {
+                    $pflag++;
+                }
+                if (empty($_POST['quantity'][$count1]) || empty($_POST['product'][$count1])) {
+                    $pflagfail++;
+                }
+            }
             if (empty($_POST['cus_username'])) {
                 $flag = 1;
                 $message = 'Please select a Username.';
-            } else if (empty($_POST['product'][0])) {
+            } else if ($pflag == 0 || $pflagfail > 0) {
                 $flag = 1;
-                $message = 'Please select a product.';
-            } else if (empty($_POST['quantity'][0])) {
+                $message = "Please Select a Product together with Quantity.";
+            } else if (count($_POST['product']) !== count(array_unique($_POST['product']))) {
                 $flag = 1;
-                $message = 'Please select a quantity.';
+                $message = "You can't select the same product multiple times";
             }
+
+
+
+
 
             try {
                 // insert query
@@ -85,7 +107,7 @@
                 if ($flag == 0) {
                     if ($stmt->execute()) {
                         $last_id = $con->lastInsertId();
-                        for ($count = 0; $count < 3; $count++) {
+                        for ($count = 0; $count < count($_POST['product']); $count++) {
                             $query2 = "INSERT INTO order_details SET order_id=:order_id, product_id=:product_id, quantity=:quantity";
                             $stmt = $con->prepare($query2);
                             $stmt->bindParam(':order_id', $last_id);
@@ -123,8 +145,11 @@
                     echo  '<option class="bg-white" disable selected value>Select Your Username</option>';
                     while ($row = $cu->fetch(PDO::FETCH_ASSOC)) {
                         extract($row);
-                        echo "<option class='bg-white' value='" . $row['username'] . "'>" . $row['username'] . "</option>";
+                        $selected = $username == $_POST['cus_username'] ? 'selected' : ''; // if user input a username then it will store in selected($username == to $_POST['cus_username']), then output after save, if no then it will not store any thing which is ' '.
+                        echo "<option class='bg-white' value='" . $username . "' $selected>" . $username . "</option>";
                     }
+
+
                     echo "</td>";
                     ?>
                 </tr>
@@ -133,37 +158,29 @@
                     <th>Quantity</th>
                 </tr>
                 <?php
+
                 $quantity = 1;
 
-                $product_arrID = array();
-                $product_arrName = array();
-                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    array_push($product_arrID, $row['product_id']);
-                    array_push($product_arrName, $row['name']);
-                }
-
-
-                for ($x = 0; $x <= 2; $x++) {
-                    echo "<tr>";
-                    echo '<td>
+                echo "<tr class='productrow'>";
+                echo '<td>
                        <select class="fs-4 rounded" name="product[]">';
-                    echo  '<option disable selected value>Select Product</option>';
-                    for ($product_count = 0; $product_count < count($product_arrName); $product_count++) {
-                        echo  "<option value='" . $product_arrID[$product_count] . "'>" . $product_arrName[$product_count] . "</option>";
-                    }
-                    echo "</select>";
-                    echo '</td>';
-
-
-                    echo "<td>";
-                    echo '<select class="w-100 fs-4 rounded" name="quantity[]" class="form-control">';
-                    echo "<option class='bg-white' disable selected value> Select Quantity</option>";
-                    for ($quantity = 1; $quantity <= 10; $quantity++) {
-                        echo "<option value='$quantity'>$quantity</option>";
-                    }
-                    echo '</td>';
-                    echo "</tr>";
+                echo  '<option disable selected value>Select Product</option>';
+                for ($product_count = 0; $product_count < count($product_arrName); $product_count++) {
+                    echo  "<option value='" . $product_arrID[$product_count] . "'>" . $product_arrName[$product_count] . "</option>";
                 }
+                echo "</select>";
+                echo '</td>';
+
+
+                echo "<td>";
+                echo '<select class="w-100 fs-4 rounded" name="quantity[]" class="form-control">';
+                echo "<option class='bg-white' disable selected value> Select Quantity</option>";
+                for ($quantity = 1; $quantity <= 10; $quantity++) {
+                    echo "<option value='$quantity'>$quantity</option>";
+                }
+                echo '</td>';
+                echo "</tr>";
+
                 ?>
 
                 <tr>
@@ -175,12 +192,34 @@
                 </tr>
             </table>
         </form>
-
+        <div class="d-flex justify-content-center flex-column flex-lg-row">
+            <div class="d-flex justify-content-center">
+                <button type="button" class="add_one btn mb-3 mx-2">Add More Product</button>
+                <button type="button" class="delete_one btn mb-3 mx-2">Delete Last Product</button>
+            </div>
+        </div>
     </div>
 
 
     <!-- end .container -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <script>
+        document.addEventListener('click', function(event) {
+            if (event.target.matches('.add_one')) {
+                var element = document.querySelector('.productrow');
+                var clone = element.cloneNode(true);
+                element.after(clone);
+            }
+            if (event.target.matches('.delete_one')) {
+                var total = document.querySelectorAll('.productrow').length;
+                if (total > 1) {
+                    var element = document.querySelector('.productrow');
+                    element.remove(element);
+                }
+            }
+        }, false);
+    </script>
 </body>
 
 </html>
